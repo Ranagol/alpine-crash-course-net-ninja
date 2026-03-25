8690d5f6874ea7b6700b6557398376566fcce3f7
Andor Horvat <andorhorvat@gmail.com>
2026-03-25 14:49:27 +0100

    Small corrections.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-document.addEventListener('alpine:init', () => {
-  Alpine.data('counter', () => ({
-    count: 0,
-    name: 'Mario',
+/**
+ * Checks is Alpine is initialized, then creates a new Alpine component called "counter
+ * "alpine:init" this is the event name
+ */
+document.addEventListener("alpine:init", () => {
 
-    logCount() {
-      console.log('The count is currently', this.count)
-    }
-  }))
-})
+    /**
+     * Here we create the data. The first argument is the name of the component, in this case 
+     * "counter". The second argument is a function that returns our data object.
+     */
+	Alpine.data("counter", () => ({
+		count: 0,
+		name: "Mario",
+
+        /**
+         * data in Alpine can store fuctions too.
+         */
+		logCount() {
+			console.log("The count is currently", this.count);
+		},
+	}));
+});
