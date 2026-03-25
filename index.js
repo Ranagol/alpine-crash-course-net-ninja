df441971de6d6415087964331a485c268d7964dd
Andor Horvat <andorhorvat@gmail.com>
2026-03-26 00:30:44 +0100

    Add comments.

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 document.addEventListener('alpine:init', () => {
-  Alpine.data('signupForm', () => ({
-    username: '',
-    password: '',
-    passwordConfirm: '',
-    belt: '',
-    bio: '',
-    newsletter: true,
-    showPass: false,
-    errors: {},
+    Alpine.data('signupForm', () => ({
+        username: '',
+        password: '',
+        passwordConfirm: '',
+        belt: '',
+        bio: '',
+        newsletter: true,
+        showPass: false,
+        errors: {},
+        beltChoices: ['White', 'Yellow', 'Green', 'Blue', 'Brown', 'Black'],
 
-    validateForm() {
-      this.errors = {}
+        validateForm() {
+            this.errors = {}
 
-      if (this.username.length < 3) {
-        this.errors.username = 'Username must be at least 3 characters.'
-      }
-      if (this.password.length < 6) {
-        this.errors.password = 'Password must be at least 6 characters.'
-      }
-      if (this.password !== this.passwordConfirm) {
-        this.errors.passwordConfirm = 'Passwords do not match.'
-      }
-      if (!this.belt) {
-        this.errors.belt = 'Please select a belt color.'
-      }
-      if (this.bio.length < 10) {
-        this.errors.bio = 'Bio must be at least 10 characters.'
-      }
-    },
+            if (this.username.length < 3) {
+                this.errors.username = 'Username must be at least 3 characters.'
+            }
+            if (this.password.length < 6) {
+                this.errors.password = 'Password must be at least 6 characters.'
+            }
+            if (this.password !== this.passwordConfirm) {
+                this.errors.passwordConfirm = 'Passwords do not match.'
+            }
+            if (!this.belt) {
+                this.errors.belt = 'Please select a belt color.'
+            }
+            if (this.bio.length < 10) {
+                this.errors.bio = 'Bio must be at least 10 characters.'
+            }
+        },
 
-    submitForm($event) {
-      this.validateForm()
+        submitForm($event) {
+            this.validateForm()
 
-      if (Object.keys(this.errors).length === 0) {
-        console.log(this.username, this.password, this.belt, this.bio, this.newsletter)
+            if (Object.keys(this.errors).length === 0) {
+                console.log(this.username, this.password, this.belt, this.bio, this.newsletter)
 
-        $event.target.reset()
-      }
-    }
-  }))
+                $event.target.reset()
+            }
+        }
+    }))
 })
